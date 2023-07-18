f8d838837b1acf65930f2ffef834dc311a814130
Nikita Sokolov <mr.sokolov99@gmail.com>
2023-07-18 11:53:09 +0300

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styled from "@xstyled/styled-components";
-import { forwardRef } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
